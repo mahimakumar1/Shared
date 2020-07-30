b53ab4e96c995f3eca8eced583b6288e2f105ba9
mahimakumar1 <68937537+mahimakumar1@users.noreply.github.com>
2020-07-30 12:37:45 -0400

    Add files via upload

---
+++ Transportly/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using Transportly.Common;
+using Transportly.Engines;
+
+namespace Transportly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //These file path are in Assets folder (make sure they exist in bin)
+            var scheduleFilePath = ConfigurationManager.AppSettings["scheduleFile"];
+            var orderFilePath = ConfigurationManager.AppSettings["orderFile"];
+
+            Console.WriteLine("** User Story 1 **");
+            Console.WriteLine();
+            var scheduleEngine = new ScheduleEngine(scheduleFilePath);
+            scheduleEngine.DoWork();
+
+            Console.WriteLine();
+            
+            Console.WriteLine("** User Story 2 **");
+            Console.WriteLine();
+            var orderEngine = new OrderEngine(orderFilePath);
+            orderEngine.DoWork();
+            
+            Console.ReadLine();
+        }
+    }
+}
